ee9fea38c3a48823e3e5b645b815e1d9c41acd24
carlos roberto <carlosrobertounopar01@hotmail.com>
2022-02-20 16:37:04 -0300

    Testando strct de ordens

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,7 +2,7 @@ defmodule Exlivery.Factory do
   use ExMachina
 
   alias Exlivery.Users.User
-  alias Exlivery.Orders.Item
+  alias Exlivery.Orders.{Item, Order}
 
   def user_factory do
     %User{
@@ -20,6 +20,23 @@ defmodule Exlivery.Factory do
       category: :pizza,
       quantity: 1,
       unity_price: Decimal.new("35.5")
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "rua das bananeiras, 165",
+      itens: [
+        build(:item),
+        build(:item,
+          description: "temaki de atum",
+          category: :japonesa,
+          quantity: 2,
+          unity_price: Decimal.new("20.50")
+        )
+      ],
+      total_price: Decimal.new("76.50"),
+      user_cpf: "11111223123"
     }
   end
 end
