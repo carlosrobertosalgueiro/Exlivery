79c6a294e91e1f99998aa35339876968340fcab6
carlos roberto <carlosrobertounopar01@hotmail.com>
2022-02-24 07:53:10 -0300

    Facilitando a criação de Orders

--- lib/exlivery.ex
+++ lib/exlivery.ex
@@ -1,10 +1,14 @@
 defmodule Exlivery do
-  alias Exlivery.Users.CreateOrUpdata
-  alias Exlivery.Users.Agent, as: UsersAgent
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Orders.Agent, as: OrderAgent
+  alias Exlivery.Users.CreateOrUpdata, as: CreateOrUpdataUser
+  alias Exlivery.Orders.CreateOrUpdata, as: CreateOrUpdataOrders
 
   def start_agents() do
-    UsersAgent.start_link(%{})
+    UserAgent.start_link(%{})
+    OrderAgent.start_link(%{})
   end
 
-  defdelegate create_or_updata_user(params), to: CreateOrUpdata, as: :call
+  defdelegate create_or_updata_user(params), to: CreateOrUpdataUser, as: :call
+  defdelegate create_or_updata_order(params), to: CreateOrUpdataOrders, as: :call
 end
