27aaa97d6f8c082c574838bfd11a186ce08ffa9c
carlos roberto <carlosrobertounopar01@hotmail.com>
2022-02-25 07:12:40 -0300

    Testando user get

--- test/users/agent_test.exs
+++ test/users/agent_test.exs
@@ -1,6 +1,7 @@
 defmodule Exlivery.Users.AgentTest do
   use ExUnit.Case
   alias Exlivery.Users.Agent, as: UsersAgent
+  alias Exlivery.Users.User
 
   import Exlivery.Factory
 
@@ -11,6 +12,40 @@ defmodule Exlivery.Users.AgentTest do
       UsersAgent.start_link(%{})
 
       assert UsersAgent.save(user) == :ok
+    end
+  end
+
+  describe "get/1" do
+    test "when the user is found, returns the user" do
+      UsersAgent.start_link(%{})
+
+      :user
+      |> build(cpf: "98765432100")
+      |> UsersAgent.save()
+
+      response = UsersAgent.get("98765432100")
+
+      expected_response =
+        {:ok,
+         %User{
+           address: "rua das bananeiras, 165",
+           age: 27,
+           cpf: "98765432100",
+           email: "carlos@hotmail.com",
+           name: "carlos"
+         }}
+
+      assert response == expected_response
+    end
+
+    test "when the user is found, returns an error" do
+      UsersAgent.start_link(%{})
+
+      response = UsersAgent.get("00000000000")
+
+      expected_response = {:error, "user not found"}
+
+      assert response == expected_response
     end
   end
 end
